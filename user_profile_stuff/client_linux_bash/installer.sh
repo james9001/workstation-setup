9d41cfbfe3b0e867cd32aacf4ee3ca91b396e254
james9001 <james9001@james9001.net>
2024-05-06 13:57:48 +1000

    First cut of automated installer script

---
+++ user_profile_stuff/client_linux_bash/installer.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+# Under construction lol
+
+sudo apt -y install \
+    nano \
+    git \
+    neofetch \
+    htop \
+    nvtop \
+    pip \
+    nfs-common \
+    ncdu \
+    ruby-rubygems \
+    fortune-mod \
+    lolcat \
+    tmux \
+	starship \
+	iperf3 \
+    virt-manager
+
+
+sudo gem install catsay
+
+
+# Install docker engine from apt ppa
+
+
+
+# Install nvm and a node version
+# TODO
+
+
+# Install golang toolchain
+# TODO
+
+
+#install a nerd font
+
+
+#Install pgadmin-4 from apt ppa
+
+
+flatpak install \
+    com.visualstudio.code \
+    dev.k8slens.OpenLens \
+    com.getpostman.Postman
+
+# Other stuff to automate:
+# chrome
+# flatseal
+# cheese
+# keepassxc
+# edge
