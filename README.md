c6424dd5e2e3ce00a0c5f25bc594ed3dad59203f
james9001 <james9001@james9001.net>
2024-11-07 23:03:26 +1100

    Updated installer script for client_macos_zsh

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 Note: After setting up environment, be sure to replace the `${ENV_VAR` stuff with the actual values as necessary.
 
-There is an `installer.sh` script for `client_macos_zsh` only at present.
+There is an `installer.sh` script for `client_macos_zsh` only at present. Install https://brew.sh first before running.
--- user_profile_stuff/client_macos_zsh/installer.sh
+++ user_profile_stuff/client_macos_zsh/installer.sh
@@ -3,34 +3,24 @@
 brew tap homebrew/cask-fonts
 
 brew install --casks \
-	visual-studio-code \
 	firefox \
-	iterm2 \
-	font-hack-nerd-font \
-	rectangle \
-	docker \
-	temurin \
-	eclipse-jee \
-	pgadmin4 \
 	google-chrome \
 	brave-browser \
-	bitwarden \
+	visual-studio-code \
 	openlens \
-	keybase \
-	signal \
+	bitwarden \
+	iterm2 \
+	font-hack-nerd-font \
+	docker \
 	spotify \
-	skype \
-	discord \
-	element \
+	postman \
+	utm \
+	pgadmin4 \
 	karabiner-elements \
 	cyberduck \
-	postman \
-	openvpn-connect \
-	private-internet-access \
 	vlc \
-	syncthing \
-	utm \
-	geekbench
+	geekbench \
+	rectangle
 
 brew install \
 	starship \
@@ -40,6 +30,7 @@ brew install \
 	htop \
 	neofetch \
 	ncdu \
-	iperf3
+	iperf3 \
+	nvm
 
 sudo gem install catsay
